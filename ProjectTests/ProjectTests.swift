39054a9bba695ba4acfb78ec05529b199ceaa724
StanleyNwaigwe <stanleycnwaigwe@yahoo.com>
2025-01-26 22:30:57 -0500

    Initial Commit

---
+++ ProjectTests/ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTests.swift
+//  ProjectTests
+//
+//  Created by Stanley Nwaigwe on 1/26/25.
+//
+
+import Testing
+@testable import Project
+
+struct ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
